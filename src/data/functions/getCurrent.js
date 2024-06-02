850adac494c7e54bd94ebeb5b86d1c9399551ad9
unknown <talmoshel444@gmail.com>
2024-06-02 18:05:28 +0300

    ok4

--- src/data/functions/getCurrent.js
+++ src/data/functions/getCurrent.js
@@ -1,6 +1,7 @@
 export async function getCurrent(cityKey) {
   const response = await fetch(
-    `https://dataservice.accuweather.com/currentconditions/v1/${cityKey}?apikey=eX0BvLwq6EEeVFtvBg7usc23ydzRticl`
+    `https://dataservice.accuweather.com/currentconditions/v1/${cityKey}?apikey=evfOeo7HTiSJxNySGtxtSOiQvtHlNXqi
+`
   );
 
   return response.json();
--- src/data/functions/getLocationUrl.js
+++ src/data/functions/getLocationUrl.js
@@ -1,9 +1,11 @@
 export function getLocationUrl(location, isGeoApi) {
   let url;
   if (!isGeoApi) {
-    url = `https://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=eX0BvLwq6EEeVFtvBg7usc23ydzRticl            &q=${location}`;
+    url = `https://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=evfOeo7HTiSJxNySGtxtSOiQvtHlNXqi
+            &q=${location}`;
     return url;
   }
-  url = `https://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=eX0BvLwq6EEeVFtvBg7usc23ydzRticl&q=${location.coordinates.lat}%2C${location.coordinates.lng}`;
+  url = `https://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=evfOeo7HTiSJxNySGtxtSOiQvtHlNXqi
+&q=${location.coordinates.lat}%2C${location.coordinates.lng}`;
   return url;
 }
